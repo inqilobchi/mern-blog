511137aaa2641d5b6a9985c4dab6b1c2c6097dfe
inqilobchi <hieojody@gmail.com>
2025-10-07 09:46:57 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -26,6 +26,8 @@ const app = express();
 app.use(cors({
   origin: '*', // ✅ Hamma saytlar uchun ruxsat
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], // ✅ kerakli HTTP metodlar
+  maxAgeSeconds: 3600,
+  responseHeader: ["Content-Type", "Authorization"],
   credentials: true // ✅ cookie yuborishga ruxsat (xohlasangiz false qiling)
 }));
 
